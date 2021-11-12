d840fedb2c6b24c3ee179ce26abe924c0469ee4b
silzktz <94162108+silzktz@users.noreply.github.com>
2021-11-12 19:53:20 +1300

    Update content.js

--- Developer Extension/content.js
+++ Developer Extension/content.js
@@ -1,6 +1,12 @@
 
  var script = document.createElement('script');
+  script.src = 'http:
+ var script = document.createElement('script');
   script.src = 'http://127.0.0.1:8000/shellshock.min.js';
+  script.onload = function() {
+    console.log('your script have been loaded');
+  }
+  document.body.appendChild(script);//127.0.0.1:8000/shellshock.min.js';
   script.onload = function() {
     console.log('your script have been loaded');
   }
